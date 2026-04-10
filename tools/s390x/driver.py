041c615cc79dbb57a4f8d6035bf26fb975b9605a
k8ika0s <k8ika0s@gmail.com>
2026-04-10 08:27:03 -0700

    Fix s390x matrix FFI skip metadata

--- tools/s390x/driver.py
+++ tools/s390x/driver.py
@@ -65,9 +65,23 @@ JIT_CORE_LUA_FILES = [
 JIT_CORE_FFI_LUA_FILES = {
     "tests/s390x/jit_core/ffi_call_trace.lua",
     "tests/s390x/jit_core/ffi_cdata_trace.lua",
+    "tests/s390x/jit_core/ffi_complex_vararg_call_trace.lua",
+    "tests/s390x/jit_core/ffi_fixed_call_pressure_trace.lua",
     "tests/s390x/jit_core/ffi_fixed_complex_call_trace.lua",
     "tests/s390x/jit_core/ffi_fixed_struct_call_trace.lua",
+    "tests/s390x/jit_core/ffi_fp_struct_vararg_call_trace.lua",
+    "tests/s390x/jit_core/ffi_fp_vararg_call_trace.lua",
+    "tests/s390x/jit_core/ffi_large_struct_vararg_call_trace.lua",
+    "tests/s390x/jit_core/ffi_literal_stop_same_callsite.lua",
+    "tests/s390x/jit_core/ffi_mixed_vararg_call_trace.lua",
+    "tests/s390x/jit_core/ffi_pointer_vararg_call_trace.lua",
+    "tests/s390x/jit_core/ffi_promotion_vararg_call_trace.lua",
     "tests/s390x/jit_core/ffi_ptr_call_trace.lua",
+    "tests/s390x/jit_core/ffi_stack_call_trace.lua",
+    "tests/s390x/jit_core/ffi_string_vararg_call_trace.lua",
+    "tests/s390x/jit_core/ffi_struct_vararg_call_trace.lua",
+    "tests/s390x/jit_core/ffi_vararg_call_trace.lua",
+    "tests/s390x/jit_core/ffi_width_vararg_call_trace.lua",
     "tests/s390x/jit_core/trace_event_postloop.lua",
 }
 
@@ -91,6 +105,7 @@ CALLBACK_FFI_LUA_FILES = {
 
 JIT_BE_FFI_LUA_FILES = {
     "tests/s390x/jit_be/mixed_width_ffi.lua",
+    "tests/s390x/jit_be/numeric_ops.lua",
 }
 
 SOAK_FFI_LUA_FILES = {
