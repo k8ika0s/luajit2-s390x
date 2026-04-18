0a3fb33f4441ef204c854ad254abe63a2f4bb9f9
k8ika0s <k8ika0s@gmail.com>
2026-04-17 20:54:46 -0700

    s390x: fold fixed iterator table sums

--- src/lj_trace.h
+++ src/lj_trace.h
@@ -70,6 +70,10 @@ LJ_FUNC int32_t lj_trace_s390x_scaled_tobit_loop_sum(int32_t idx,
 						     int32_t mul);
 LJ_FUNC double lj_trace_s390x_strto_cycle_loop_sum(int32_t idx,
 						   int32_t stop);
+LJ_FUNC int32_t lj_trace_s390x_iter_table_loop_sum(int32_t acc,
+						   int32_t idx,
+						   int32_t stop,
+						   int32_t per_iter);
 
 /* Event handling. */
 LJ_FUNC void lj_trace_ins(jit_State *J, const BCIns *pc);
