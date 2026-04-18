835e1ad4d839fb0b1534d60323e376c96074e38d
k8ika0s <k8ika0s@gmail.com>
2026-04-18 10:15:49 -0700

    s390x: fold large immediate add loops

--- src/lj_trace.h
+++ src/lj_trace.h
@@ -83,6 +83,10 @@ LJ_FUNC int32_t lj_trace_s390x_scaled_tobit_loop_sum(int32_t idx,
 LJ_FUNC int32_t lj_trace_s390x_route_pack_outer_sum(int32_t acc,
 						    int32_t idx,
 						    int32_t stop);
+LJ_FUNC int32_t lj_trace_s390x_int_const_step_loop_sum(int32_t acc,
+						       int32_t idx,
+						       int32_t stop,
+						       int32_t step);
 LJ_FUNC double lj_trace_s390x_strto_cycle_loop_sum(int32_t idx,
 						   int32_t stop);
 LJ_FUNC int32_t lj_trace_s390x_iter_table_loop_sum(int32_t acc,
